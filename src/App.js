7d9b91dc6a4b9c68a2b1fd2e7bbb8173a8bcb61d
Hamdan <hamdanahmadkhawaja@gmail.com>
2024-02-03 18:51:34 +0500

    First Commit, Added some front-end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New chat</button>
+        <ul className="history">
+          <li>Bugh</li>
+        </ul>
+        <nav>
+          <p>Made by Hamdan</p>
+        </nav>
+      </section>
+
+      <section className="main">
+        <h1>HamdanGPT</h1>
+        <ul className="feed"></ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div id="submit"></div>
+          </div>
+          <p className="info">
+            Chat GPT Mar 14 Version. Free Research Preview. Our goal is to make
+            AI systems more natural and safe to interact with. Your feedback
+            will help us improve.
+          </p>
+        </div>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
